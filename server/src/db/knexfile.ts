9107cc599abda74c2a283afa49532d5d2f0aa9fd
rbarbazz <raphael.barbazza@gmail.com>
2020-08-02 10:10:57 -0700

    fix: db settings prod

--- server/src/db/knexfile.ts
+++ server/src/db/knexfile.ts
@@ -5,11 +5,12 @@ export const knexConfig: { [env: string]: object } = {
     client: 'pg',
     connection: process.env.PG_CONNECTION_STRING_DEV,
     ...knexSnakeCaseMappers(),
-    pool: { min: 0, max: 4 },
+    pool: { min: 0, max: 5 },
   },
   production: {
     client: 'pg',
     connection: process.env.PG_CONNECTION_STRING_PROD,
     ...knexSnakeCaseMappers(),
+    pool: { min: 0, max: 5 },
   },
 };
